c8d227d256525b845a707f8a68eee00b0300b30d
ricardo <ricardo.correia.ferreira@gmail.com>
2023-01-04 16:02:07 +0100

    work promo

--- cubes-api/services/bddConnection.ts
+++ cubes-api/services/bddConnection.ts
@@ -1,11 +1,14 @@
 import { Sequelize, SequelizeOptions } from "sequelize-typescript";
-import config from "../config.base";
+import config from "../config";
 import { Person } from "../models/person.model";
+import { Pilot } from "../models/pilot.model";
 import { Planning } from "../models/planning.model";
+import { Promo } from "../models/promo.model";
+import { Room } from "../models/room.model";
 const sequelize: Sequelize = new Sequelize({
   ...config.bd,
   dialect: "mysql",
   models: ["./models/**/*.model.js"],
 } as SequelizeOptions);
-sequelize.addModels([Person, Planning]);
+sequelize.addModels([Person, Planning, Promo, Pilot, Room]);
 export default sequelize;
